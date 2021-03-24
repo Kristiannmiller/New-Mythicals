b8d65054f632afffed0366c5110ab53a304bc996
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-24 09:10:10 -0600

    Add completeCraft method

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -4,6 +4,10 @@ class Craft {
     this.materials = data.materials
     this.completed = false
   }
+  completeCraft() {
+    this.completed = true
+    return 'All done! It looks great!'
+  }
 }
 
 module.exports = Craft;
