a7daae9db7dec9aa51830f8edd8f534d4df601ee
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-24 09:21:52 -0600

    Add calculateProjectTotal method

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -4,6 +4,13 @@ class Craft {
     this.materials = data.materials
     this.completed = false
   }
+  calculateProjectTotal() {
+    let total = 0
+    for(let i = 0; i < this.materials.length; i++) {
+      total += this.materials[i].calculateMaterialCost()
+    }
+    return total
+  }
   completeCraft() {
     this.completed = true
     return 'All done! It looks great!'
