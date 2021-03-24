30b0032d41ff2f932b1ec60774a2d5d61e7a25c8
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-24 09:07:28 -0600

    Add basic properties

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,10 @@
 class Material {
-
+  constructor(type, price, amount, unit) {
+    this.name = type
+    this.price = price
+    this.amount = amount
+    this.units = unit
+  }
 }
 
 module.exports = Material;
