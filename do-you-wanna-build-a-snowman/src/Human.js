ef026e8b30bf1d4d05a3f91dbe3020f8c2130cd8
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-25 08:27:06 -0600

    Add placeMagicHat method

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -16,7 +16,10 @@ class Human {
   }
   buildASnowman() {
     return new Snowman(this.materials)
-
+  }
+  placeMagicHat() {
+    let snowman = this.buildASnowman()
+    return snowman.canWearMagicHat() ? 'Woah, this snowman is coming to life!' : 'I guess I didn\'t build it correctly.'
   }
 }
 
