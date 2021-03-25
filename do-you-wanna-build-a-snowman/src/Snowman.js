7412ad6734af5e36c68710eb79d6665fe0732b90
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-25 08:15:09 -0600

    Add materials to constructor

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,10 @@
 class Snowman {
-
+  constructor(materials) {
+    this.carrots = materials.carrots
+    this.coal = materials.coal
+    this.buttons = materials.buttons
+    this.snowballs = materials.snowballs
+  }
 }
 
 module.exports = Snowman;
