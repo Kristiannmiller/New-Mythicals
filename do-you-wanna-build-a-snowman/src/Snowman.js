be2c9501a628d3b19d3cfa3b601df6e887ba783e
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-25 08:26:46 -0600

    Add return statement to canWearMagicHat method

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -9,7 +9,9 @@ class Snowman {
   canWearMagicHat() {
     if(this.coal > 1 && this.buttons > 4 && this.carrots > 0 && this.snowballs > 1) {
       this.magicHat = true
+      return true
     }
+    return false
   }
 }
 
