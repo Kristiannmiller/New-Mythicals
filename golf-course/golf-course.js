1d38642669f4786553d1ba2bce48d062cae11a8e
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-26 16:46:13 -0600

    Add conditional to checkInGroup method

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -7,9 +7,14 @@ class GolfCourse {
     this.currentlyPlaying = []
   }
   checkInGroup(group) {
-    for(let i=0; i < group.length; i++) {
-      this.currentlyPlaying.unshift(group[i].name)
-      this.openings--
+    if(group.length <= this.openings) {
+      for(let i=0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name)
+        this.openings--
+      }
+      return 'You\'re checked in. Have fun!'
+    } else {
+      return 'Sorry, we are currently booked! Please come back later.'
     }
   }
 }
