43e0bc3c6d77b3fd3be8143b3a1de148b3fa1eb7
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-28 13:00:31 -0600

    Add adjustCrankyness method

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,12 +4,19 @@ class Centaur {
     this.breed = stats.type
     this.cranky = false
     this.standing = true
+    this.crankyCount = 0
   }
   shootBow() {
+    this.adjustCrankyness()
     return 'Twang!!!'
   }
   run() {
+    this.adjustCrankyness()
     return 'Clop clop clop clop!!!'
+  }
+  adjustCrankyness() {
+    this.crankyCount++
+    if(this.crankyCount > 2) this.cranky = true
   }
 }
 module.exports = Centaur
