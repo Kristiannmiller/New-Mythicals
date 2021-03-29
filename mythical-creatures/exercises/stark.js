3f5f29cd2f70d3c1efe015fa81befb5751323210
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-29 08:39:43 -0600

    Add safe to constructor

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -2,6 +2,7 @@ class Stark {
   constructor(data = {name: 'Larry', area: 'Winterfell'}) {
     this.name = data.name
     this.location = data.area || 'Winterfell'
+    this.safe = false
   }
 }
 module.exports = Stark
