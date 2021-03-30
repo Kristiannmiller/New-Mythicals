2d6c83244687ce354ac0c87c9b37de64db0f2b58
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-30 09:44:44 -0600

    Add eat method

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -4,9 +4,16 @@ class Dragon {
     this.name = name
     this.rider = rider
     this.hungry = true
+    this.meal = 0
   }
   greet() {
     return `Hi, ${this.rider}!`
+  }
+  eat() {
+    this.meal++
+    if(this.meal > 2) {
+      this.hungry = false
+    }
   }
 }
 module.exports = Dragon
