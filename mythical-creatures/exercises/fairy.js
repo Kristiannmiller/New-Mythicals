c05b9e4b42f9124f6a7be13cf0f19f6a715e44ef
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-31 09:30:10 -0600

    Add humanWards property

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -4,6 +4,7 @@ class Fairy {
     this.dust = 10
     this.clothes = {dresses: ['Iris']}
     this.disposition = 'Good natured'
+    this.humanWards = []
   }
   receiveBelief() {
     this.dust++
@@ -21,6 +22,7 @@ class Fairy {
   }
   replaceInfant(infant) {
     if(this.disposition === 'Vengeful') {
+      this.humanWards.push(infant)
       infant.disposition = 'Malicious'
     }
     return infant
