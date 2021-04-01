f4bafd444278fdbe8f101e29fbb4bfecbf69aeaf
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-01 11:28:33 -0600

    Add getRing method

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -4,6 +4,7 @@ class Hobbit {
     this.age = 0
     this.adult = false
     this.old = false
+    this.hasRing = false
   }
   celebrateBirthday() {
     this.age++
@@ -12,6 +13,14 @@ class Hobbit {
     }
     if(this.age > 100) {
       this.old = true
+    }
+  }
+  getRing() {
+    if(this.name === 'Frodo') {
+      this.hasRing = true
+      return 'Here is the ring!'
+    } else {
+      return 'You can\'t have it!'
     }
   }
 }
