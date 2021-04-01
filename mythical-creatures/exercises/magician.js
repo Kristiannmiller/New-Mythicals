65289cdc84ae3b0102a8d94450795b8eba4d5fca
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-01 11:36:40 -0600

    Add conditional to favoriteAccessory property

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,7 +2,7 @@ class Magician {
   constructor(data) {
     this.name = `The Great ${data.name}`
     this.assistant = data.assistant
-    this.favoriteAccessory = 'top hat'
+    this.favoriteAccessory = data.clothing || 'top hat'
   }
 }
 module.exports = Magician
