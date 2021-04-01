49db7d3d1b5c717be1e920f792cacfb3b6bec6d3
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-01 11:24:06 -0600

    Add adult property

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,9 +2,13 @@ class Hobbit {
   constructor(data) {
     this.name = data.name
     this.age = 0
+    this.adult = false
   }
   celebrateBirthday() {
     this.age++
+    if(this.age > 32) {
+      this.adult = true
+    }
   }
 }
 module.exports = Hobbit
