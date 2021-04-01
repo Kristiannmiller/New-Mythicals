147faf62b48066a5e9556a725c94e0f9f9a400d6
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-01 11:35:39 -0600

    Add favoriteAccessory property

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,6 +2,7 @@ class Magician {
   constructor(data) {
     this.name = `The Great ${data.name}`
     this.assistant = data.assistant
+    this.favoriteAccessory = 'top hat'
   }
 }
 module.exports = Magician
