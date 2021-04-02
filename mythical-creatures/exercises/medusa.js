4fce0f3cbaeece0c1d7f740552cc80477134b848
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-02 10:10:52 -0600

    Adjust conditional to push out first statue when full in gazeAtVictim method

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -8,9 +8,10 @@ class Medusa {
   }
   gazeAtVictim(victim) {
     let newStatue = new Statue(victim.name)
-    if(this.statues.length < 3) {
-      this.statues.push(newStatue)
+    if(this.statues.length > 2) {
+      this.statues.shift()
     }
+    this.statues.push(newStatue)
   }
 }
 module.exports = Medusa
