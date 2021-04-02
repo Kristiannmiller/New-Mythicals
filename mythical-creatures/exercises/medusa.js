ec46acf897bccbe57953692032ab3609603c4210
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-02 10:07:04 -0600

    Add gazeAtVictim method

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -6,5 +6,9 @@ class Medusa {
     this.name = name
     this.statues = []
   }
+  gazeAtVictim(victim) {
+    let newStatue = new Statue(victim.name)
+    this.statues.push(newStatue)
+  }
 }
 module.exports = Medusa
