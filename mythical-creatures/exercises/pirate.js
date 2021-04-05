ce887b143fbc959ad55e29000aac60887111f8e1
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-05 09:23:34 -0600

    Add curse conditional to robShip method

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -6,8 +6,13 @@ class Pirate {
     this.booty = 0
   }
   robShip() {
-    this.booty += 100
-    return 'YAARRR!'
+    if(this.booty >= 500) {
+      this.cursed = true
+      return 'ARG! I\'ve been cursed!'
+    } else {
+      this.booty += 100
+      return 'YAARRR!'
+    }
   }
 }
 module.exports = Pirate
