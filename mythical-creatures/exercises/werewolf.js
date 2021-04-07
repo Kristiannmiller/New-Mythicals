337d7527a9efaa811472749d8a013248f26ff186
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-07 08:00:08 -0600

    Add conditional to completeTransformation method

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -4,8 +4,13 @@ class Werewolf {
     this.form = 'human'
   }
   completeTransformation() {
-    this.form = 'wolf'
-    return 'Aaa-Woooo!'
+    if(this.form === 'wolf') {
+      this.form = 'human'
+      return 'Where are I?'
+    } else {
+      this.form = 'wolf'
+      return 'Aaa-Woooo!'
+    }
   }
 }
 module.exports = Werewolf
