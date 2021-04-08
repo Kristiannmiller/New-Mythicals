b1ceddfb907ca19430badb0f892ff507c2f993be
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-08 13:22:05 -0600

    Add conditional to repair method

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -2,12 +2,19 @@ class Part {
   constructor(type) {
     this.type = type
     this.broken = false
+    this.repairsMade = 0
   }
   break() {
     this.broken = true
   }
   repair() {
-    this.broken = false
+    if(this.repairsMade < 1) {
+      this.repairsMade++
+      this.broken = false
+    } else {
+      return `This ${this.type} has seen better days, we\'ll need a brand new one!`
+    }
+
   }
 }
 
