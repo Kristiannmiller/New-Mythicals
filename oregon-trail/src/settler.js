d00b8548cc1d94d69d50a6733663e31d270091f9
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-08 13:24:51 -0600

    Add conditional to nationality property

--- oregon-trail/src/settler.js
+++ oregon-trail/src/settler.js
@@ -2,7 +2,7 @@ class Settler {
   constructor(data) {
     this.name = data.name
     this.age = data.age
-    this.nationality = 'unknown'
+    this.nationality = data.nationality || 'unknown'
   }
 }
 module.exports = Settler
