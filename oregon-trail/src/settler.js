434177a298d861f5db300302994c302801e36d5d
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-08 13:25:20 -0600

    Add status property

--- oregon-trail/src/settler.js
+++ oregon-trail/src/settler.js
@@ -3,6 +3,7 @@ class Settler {
     this.name = data.name
     this.age = data.age
     this.nationality = data.nationality || 'unknown'
+    this.status = 'healthy'
   }
 }
 module.exports = Settler
