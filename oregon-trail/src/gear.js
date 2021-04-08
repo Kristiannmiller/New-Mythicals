87329dc9e96aec6a30a35409b519f38967a4aa9e
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-08 13:17:45 -0600

    Add checkForValidity method

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,7 +1,19 @@
 class Gear {
   constructor(type, quantity) {
+    this.originalType = type
     this.type = type
     this.quantity = quantity
+  }
+  checkForValidity() {
+    var validTypes = ['food', 'clothes',
+  'ammunition']
+    for(var i = 0; i < validTypes.length; i++) {
+      if(this.originalType === validTypes[i]) {
+        return `Great, we\'ll need lots of ${this.type}!`
+      }
+    }
+    this.type = null
+    return `I don\`t think a ${this.originalType} will help us.`
   }
 }
 
