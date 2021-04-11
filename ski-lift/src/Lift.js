daf9dc15c55e76ff767677e4a0a26d59ad80de9c
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-11 12:03:38 -0600

    Add conditional to return statement in admitSkier method

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -8,11 +8,11 @@ class Lift {
     this.safetyBar = 'up'
   }
   admitSkier(name, ticket) {
-    if(this.skiers.length < this.limit) {
+    if(this.skiers.length < this.limit && ticket) {
       let skierToAdd = new Skier(name, ticket)
       this.skiers.push(skierToAdd)
     } else {
-      return `Sorry, ${name}. Please wait for the next lift!`
+      return ticket ? `Sorry, ${name}. Please wait for the next lift!` : `Sorry, ${name}. You need a lift ticket!`
     }
   }
 }
