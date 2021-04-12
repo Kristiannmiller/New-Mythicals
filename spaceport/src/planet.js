4c9cfcb2389e84bad955d5a3d68ee4180426f43f
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-12 09:16:21 -0600

    Create landShip method

--- spaceport/src/planet.js
+++ spaceport/src/planet.js
@@ -2,6 +2,10 @@ class Planet {
   constructor(data) {
     this.name = data.name
     this.shop = data.shop
+    this.currentShip = undefined
+  }
+  landShip(ship) {
+    this.currentShip = ship
   }
 }
 module.exports = Planet
