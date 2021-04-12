81888556bc9f8b9c57c770e9ade6d9425799b5b4
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-12 09:24:26 -0600

    Create calculateDistance method

--- spaceport/src/planet.js
+++ spaceport/src/planet.js
@@ -8,5 +8,11 @@ class Planet {
   landShip(ship) {
     this.currentShip = ship
   }
+  calculateDistance(otherPlanet) {
+    var xDiff = Math.pow(otherPlanet.coordinates.x - this.coordinates.x, 2)
+    var yDiff = Math.pow(otherPlanet.coordinates.y - this.coordinates.y, 2)
+    var zDiff = Math.pow(otherPlanet.coordinates.z - this.coordinates.z, 2)
+    return Math.sqrt(xDiff + yDiff + zDiff)
+  }
 }
 module.exports = Planet
