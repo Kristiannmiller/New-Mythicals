6d9db1b3d1dc964a8f576b4ea7a675aa7c5a4e0a
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-13 07:52:02 -0600

    Add contains method

--- trick-or-treat/lib/bag.js
+++ trick-or-treat/lib/bag.js
@@ -5,8 +5,17 @@ class Bag {
     this.candies = []
   }
   fill(candy) {
+    this.empty = false
     this.count++
     this.candies.push(candy)
+  }
+  contains(candy) {
+    for(let i = 0; i < this.candies.length; i++) {
+      if(this.candies[i].type === candy) {
+        return true
+      }
+    }
+    return false
   }
 }
 module.exports = Bag
