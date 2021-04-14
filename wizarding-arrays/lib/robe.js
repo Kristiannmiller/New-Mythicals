b88e19d07f0bbaa092744f3c928a0c06566f89c6
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-14 08:00:46 -0600

    Create retrieve method

--- wizarding-arrays/lib/robe.js
+++ wizarding-arrays/lib/robe.js
@@ -2,11 +2,19 @@ class Robe {
   constructor() {
     this.pockets = [[],[],[],[],[],[],[],[],[],[]]
   }
-  addToPocket(object) {
+  addToPocket(item) {
     for(let i=0; i<this.pockets.length; i++) {
       if(this.pockets[i].length === 0) {
-        this.pockets[i].push(object)
+        this.pockets[i].push(item)
         return
+      }
+    }
+  }
+  retrieve(item) {
+    for(let i=0; i<this.pockets.length; i++) {
+      if(this.pockets[i].includes(item)) {
+        this.pockets[i] = []
+        return item
       }
     }
   }
