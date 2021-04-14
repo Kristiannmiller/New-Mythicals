a4716d69e61b5b8d9581712c5a7be038bdce3ec8
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-14 07:51:32 -0600

    Add exectutionHistory prop

--- wizarding-arrays/lib/spell.js
+++ wizarding-arrays/lib/spell.js
@@ -2,14 +2,17 @@ class Spell {
   constructor(name, description) {
     this.name = name
     this.description = description
+    this.executionHistory = []
   }
   execute(target) {
     let phrase = []
     if(Array.isArray(target)) {
       for(let i=0; i<target.length; i++) {
         phrase.push(`Success! You've cast a spell on the ${target[i]}.`)
+        this.executionHistory.push(target[i])
       }
     } else {
+      this.executionHistory.push(target)
       return `Success! You've cast a spell on the ${target}.`
     }
     return phrase.join(" ")
