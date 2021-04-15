f0e2227209e873365c97b1a438dfe70665a33716
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-15 07:50:50 -0600

    Create adoptPet method

--- wizarding-objects/lib/wizard.js
+++ wizarding-objects/lib/wizard.js
@@ -1,6 +1,13 @@
 class Wizard {
   constructor(data) {
     this.name = data.name
+    this.petsCount = 0
+    this.pets = []
   }
+  adoptPet(pet) {
+    this.petsCount++
+    this.pets.push(pet)
+  }
+
 }
 module.exports = Wizard
