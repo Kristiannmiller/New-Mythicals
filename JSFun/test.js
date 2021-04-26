600410cd0fac472a6adfe111cbfa6e9037f1e5b3
Kristi Miller <kristi.ann.miller@gmail.com>
2021-04-25 21:01:02 -0600

    Adjust spacing

--- JSFun/test.js
+++ JSFun/test.js
@@ -333,7 +333,7 @@ describe("PROTOTYPES", () => {
   });
 
   describe("National Park Prompts", () => {
-    it.skip("getParkVisitList", () => {
+    it("getParkVisitList", () => {
       const e = nationalParksPrompts.getParkVisitList();
 
       expect(e).to.deep.equal({
